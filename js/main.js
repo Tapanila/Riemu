1faa19f25ba6a4cedf524c3d10b1bcfda7484654
“tatu <koivisto.tatu@gmail.com>
2017-05-27 20:57:37 +0300

    send image

--- js/main.js
+++ js/main.js
@@ -1,6 +1,29 @@
 $(document).ready(function () {
 
-var inputElement = $('#upload-photo');
-console.log(inputElement);
+var file;
+
+document.getElementById('upload-photo').addEventListener('change', function(){
+    file = this.files[0];
+    console.log("name : " + file.name);
+    console.log("size : " + file.size);
+    console.log("type : " + file.type);
+    console.log(file);
+    uploadFile(file);
+}, false);
+
+function uploadFile(file){
+    var url = 'http://riemu-api.azurewebsites.net/api/happy';
+    var xhr = new XMLHttpRequest();
+    var fd = new FormData();
+    xhr.open("POST", url, true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            // Every thing ok, file uploaded
+            console.log(xhr.responseText); // handle response.
+        }
+    };
+    fd.append("upload_file", file);
+    xhr.send(fd);
+}
 
 });
